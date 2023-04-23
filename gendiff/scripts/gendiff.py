b5f03a47ca2abcc8a23d2f9543572ab3925b9f81
Kirill Tanaev <kirilltanae314@gmail.com>
2023-04-23 22:53:02 +0600

    add help output

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
 
+import argparse
+
+
 def main():
-    print('it work!')
+    parser = argparse.ArgumentParser(
+                        prog='gendiff',
+                        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == '__main__':
